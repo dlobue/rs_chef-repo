f13993d473a8e93a57c7c8139f3b4d29a23cab43
Dominic LoBue <dominic.lobue@gmail.com>
2010-12-21 19:24:54 -0800

    lets try this again

--- cookbooks/rs_exercise/recipes/tagtest.rb
+++ cookbooks/rs_exercise/recipes/tagtest.rb
@@ -1,13 +1,14 @@
 
 
-log("getting all machine tags"){ level :debug }
+log "getting all machine tags"
 
 server_collection "all" do
     tags nil
 end
 
-log("got 'em"){ level :debug }
+log "got 'em: #{@node[:server_collection]['all']}"
 
+log "now to see what we got"
 ruby_block "pprinter" do
     action :create
     block do
@@ -15,4 +16,5 @@ ruby_block "pprinter" do
         pp @node[:server_collection]["all"]
     end
 end
+log "done printing what was found"
 
