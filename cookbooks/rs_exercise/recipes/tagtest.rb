24981058098c2b3794466e1ddcbd13df25006d54
Dominic LoBue <dominic.lobue@gmail.com>
2010-12-21 20:42:59 -0800

    more test changes

--- cookbooks/rs_exercise/recipes/tagtest.rb
+++ cookbooks/rs_exercise/recipes/tagtest.rb
@@ -3,7 +3,7 @@
 log "getting all machine tags"
 
 server_collection "all" do
-    tags nil
+    tags "*"
 end
 
 log "got 'em: #{@node[:server_collection]['all']}"
@@ -12,8 +12,9 @@ log "now to see what we got"
 ruby_block "pprinter" do
     action :create
     block do
-        require 'pp'
-        pp @node[:server_collection]["all"]
+        @node[:server_collection]["all"].each_pair do |k,v1|
+            log "key: #{k} value: #{v1.inspect}"
+        end
     end
 end
 log "done printing what was found"
