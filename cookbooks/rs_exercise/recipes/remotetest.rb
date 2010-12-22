ccb1bcc74977c07c0e3fc88e01b37dbbb63aace1
Dominic LoBue <dominic.lobue@gmail.com>
2010-12-22 15:16:12 -0800

    more experimental changes

--- cookbooks/rs_exercise/recipes/remotetest.rb
+++ cookbooks/rs_exercise/recipes/remotetest.rb
@@ -1,6 +1,7 @@
 
 remote_recipe "make noise somewhere" do
     recipe "rs_exercise::noise"
-    recipients_tags "rs_login:state=active"
+    #recipients_tags "rs_login:state=active"
+    recipients_tags 'dns_server:type=master'
 end
 
