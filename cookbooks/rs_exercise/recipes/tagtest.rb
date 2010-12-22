f7e92495c50bc44a5b84a1df565a75dfe87527a3
Dominic LoBue <dominic.lobue@gmail.com>
2010-12-22 13:23:53 -0800

    more experimental changes

--- cookbooks/rs_exercise/recipes/tagtest.rb
+++ cookbooks/rs_exercise/recipes/tagtest.rb
@@ -6,20 +6,20 @@ server_collection "all" do
     tags 'rs_login:state=active'
 end
 
-#log "got 'em"
+log "got 'em"
 #log "got 'em: #{@node.inspect}"
-log "got 'em: #{@node[:server_collection]['all'].inspect}"
-log "got 'em: #{@node['server_collection']['all'].inspect}"
+#log "got 'em: #{@node[:server_collection]['all'].inspect}"
+#log "got 'em: #{@node['server_collection']['all'].inspect}"
 
-#log "now to see what we got"
-#ruby_block "pprinter" do
-#    action :create
-#    block do
-#        require 'yaml'
-#        File.open( '/tmp/tagoutput.yaml', 'w') do |out|
-#            YAML.dump( @node["server_collection"], out )
-#        end
-#    end
-#end
-#log "done printing what was found"
-#
+log "now to see what we got"
+ruby_block "pprinter" do
+    action :create
+    block do
+        require 'pp'
+        File.open( '/tmp/tagoutput.pp', 'w') do |out|
+            pp( @node["server_collection"], out )
+        end
+    end
+end
+log "done printing what was found"
+
