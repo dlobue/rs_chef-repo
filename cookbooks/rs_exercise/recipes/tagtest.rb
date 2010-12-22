974c8fa609a80aea8b50088f6c15b0913c2c6329
Dominic LoBue <dominic.lobue@gmail.com>
2010-12-22 12:03:44 -0800

    more experimental changes

--- cookbooks/rs_exercise/recipes/tagtest.rb
+++ cookbooks/rs_exercise/recipes/tagtest.rb
@@ -6,16 +6,17 @@ server_collection "all" do
     tags 'rs_login:state=active'
 end
 
-log "got 'em: #{@node[:server_collection]}"
+log "got 'em: #{@node.inspect}"
+#log "got 'em: #{@node[:server_collection]}"
 
-log "now to see what we got"
-ruby_block "pprinter" do
-    action :create
-    block do
-        @node[:server_collection].each_pair do |k,v1|
-            log "key: #{k} value: #{v1.inspect}"
-        end
-    end
-end
-log "done printing what was found"
-
+#log "now to see what we got"
+#ruby_block "pprinter" do
+#    action :create
+#    block do
+#        @node[:server_collection].each_pair do |k,v1|
+#            log "key: #{k} value: #{v1.inspect}"
+#        end
+#    end
+#end
+#log "done printing what was found"
+#
